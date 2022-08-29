5059b1f6419318e4c2ff0ea1f4fc9b1103687bcd
traaitt | TRRXITTE inc <traaitt@icloud.com>
2022-08-29 20:24:44 +0200

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,19 @@
+const ExplorerConfig = {
+  apiBaseUrl: 'https://cacheapi.traaittcash.com',
+  ticker: 'XTCASH',
+  maxSupply: 8874400000000000,
+  emissionSpeed: 25,
+  decimalPoints: 8,
+  blockTargetTime: 144,
+  startYear: 2020,
+  genesisTimestamp: 1582814823,
+  emissionCurveInterval: 20000,
+  emissionCurveDataPoints: 15000,
+  addressPrefix: 925524,
+  forkHeights: [
+    1, // 1
+    17000000, // 2
+    43000000, // 3
+    68000000 // 4
+  ]
+}
